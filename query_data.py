64d19aedcc49a8fcfda6e7b6a6ad799d2998f68a
Shirley Crenshaw <crenshs@gmail.com>
2024-10-27 14:00:11 -0500

    pysqlite3-binary

--- query_data.py
+++ query_data.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import argparse
 from langchain_chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
